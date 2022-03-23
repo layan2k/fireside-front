816303d55c67790c9eb85fde3e78b5561284d7b7
layan2k <lesliedeeshumba@gmail.com>
2022-03-23 13:29:21 +0200

    locchange

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,10 @@ export default function Document() {
         <Html>
           <Head>
           <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css' />
-          <link rel="apple-touch-icon" sizes="180x180" href="../assets/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon-16x16.png"/>
-          <link rel="manifest" href="../assets/site.webmanifest"></link>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"></link>
           </Head>
           <body>
             <Main />
